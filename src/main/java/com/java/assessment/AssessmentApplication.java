d34da9532a91a23a75a289b1f59672dcac66bb66
nethmih <nethmihettiarachchi484@gmail.com>
2023-03-25 18:05:27 +0800

    formatting the file

--- src/main/java/com/java/assessment/AssessmentApplication.java
+++ src/main/java/com/java/assessment/AssessmentApplication.java
@@ -3,7 +3,6 @@ package com.java.assessment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class AssessmentApplication {
 	public static void main(String[] args) {
